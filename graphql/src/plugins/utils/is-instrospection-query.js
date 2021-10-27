7c9b43659948a2caa4b3f489bce88837145abc0a
Jacob Bare <jacob@parameter1.com>
2021-10-27 14:35:21 -0500

    Ensure operation and selectionSet are present

--- graphql/src/plugins/utils/is-instrospection-query.js
+++ graphql/src/plugins/utils/is-instrospection-query.js
@@ -3,6 +3,7 @@
  */
 export default function isIntrospectionQuery(requestContext) {
   const { operation } = requestContext;
+  if (!operation || !operation.selectionSet) return false;
   return operation.selectionSet.selections.every((selection) => {
     const fieldName = selection.name.value;
     return fieldName.startsWith('__');
