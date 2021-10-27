a71e12914b7d58b9e03748832fa785b7d188f59c
Jacob Bare <jacob@parameter1.com>
2021-10-27 13:47:23 -0500

    Convert client into class

--- packages/mongodb/src/client.js
+++ packages/mongodb/src/client.js
@@ -1,25 +1,53 @@
 import { MongoClient } from 'mongodb';
 
-/**
- *
- * @param {object} params
- * @param {string} params.url The database URL to connect to
- * @param {MongoClientOptions} [params.options={}] Options to pass to `MongoClient.connect`
- */
-export default function MongoDBClient({ url, options: clientOptions } = {}) {
-  const client = new MongoClient(url, clientOptions);
-  let connectPromise;
+export default class MongoDBClient {
+  /**
+   *
+   * @param {object} params
+   * @param {string} params.url The database URL to connect to
+   * @param {MongoClientOptions} [params.options={}] Options to pass to `MongoClient.connect`
+   */
+  constructor({ url, options } = {}) {
+    this.url = url;
+    this.options = options;
+    this.client = new MongoClient(url, options);
+  }
+
+  /**
+   * Closes the connection to the server.
+   *
+   * @param {boolean} [force] Whether to forcefully close the connection.
+   * @return {Promise<void>}
+   */
+  async close(force) {
+    if (!this.promise) return null;
+    await this.promise;
+    return this.client.close(force);
+  }
+
+  /**
+   * Fetch a specific collection.
+   *
+   * @param {object} params
+   * @param {string} params.dbName The database name
+   * @param {string} params.name The collection name
+   * @param {object} [params.options={}] Options to pass to the `Db.collection` method
+   * @return {Promise<mongodb.Collection>}
+   */
+  async collection({ dbName, name, options = {} } = {}) {
+    const db = await this.db({ name: dbName });
+    return db.collection(name, options);
+  }
 
   /**
    * Connects to the server.
    *
-   * @returns {Promise<MongoClient>}
+   * @return {Promise<MongoClient>}
    */
-  const connect = async () => {
-    if (!connectPromise) connectPromise = client.connect();
-    await connectPromise;
-    return client;
-  };
+  async connect() {
+    if (!this.promise) this.promise = this.client.connect(this.url, this.options);
+    return this.promise;
+  }
 
   /**
    * Create a new Db instance sharing the current socket connections.
@@ -29,45 +57,8 @@ export default function MongoDBClient({ url, options: clientOptions } = {}) {
    * @param {object} params.options Options to pass to the `MongoClient.db` method
    * @return {Promise<Db>}
    */
-  const db = async ({ name, options } = {}) => {
-    await connect();
-    return client.db(name, options);
-  };
-
-  /**
-   * Fetch a specific collection.
-   *
-   * @param {object} params
-   * @param {string} params.dbName The database name
-   * @param {string} params.name The collection name
-   * @param {object} [params.options={}] Options to pass to the `Db.collection` method
-   * @return {Promise<Collection>}
-   */
-  const collection = async ({
-    dbName,
-    name,
-    options = {},
-  } = {}) => {
-    const dbInstance = await db({ name: dbName });
-    return dbInstance.collection(name, options);
-  };
-
-  /**
-   * Closes the connection to the server.
-   *
-   * @param {boolean} [force] Whether to forcefully close the connection.
-   * @return {Promise<void>}
-   */
-  const close = async (force) => {
-    if (!connectPromise) return null;
-    await connect();
-    return client.close(force);
-  };
-
-  return {
-    connect,
-    db,
-    collection,
-    close,
-  };
+  async db({ name, options } = {}) {
+    await this.connect();
+    return this.client.db(name, options);
+  }
 }
