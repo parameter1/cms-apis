1261099d225d4dd4dd9892a1b21f02e3534dcee8
Jacob Bare <jacob@parameter1.com>
2021-10-28 11:28:32 -0500

    Export `ObjectId`

--- packages/mongodb/src/index.js
+++ packages/mongodb/src/index.js
@@ -1,3 +1,4 @@
+export { ObjectId } from 'mongodb';
 export { default as MongoDBClient } from './client.js';
 export { default as Repo } from './repo.js';
 export { default as filterMongoURL } from './utils/filter-url.js';
