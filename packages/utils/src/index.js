f7edcf6697dc32dafbc3ec84f7e36ed376007422
Jacob Bare <jacob@parameter1.com>
2021-10-29 15:39:13 -0500

    Create ucfirst util

--- packages/utils/src/index.js
+++ packages/utils/src/index.js
@@ -5,4 +5,5 @@ export { default as immediatelyThrow } from './immediately-throw.js';
 export { default as isFunction } from './is-function.js';
 export { default as isObject } from './is-object.js';
 export { default as trim } from './trim.js';
+export { default as ucFirst } from './uc-first.js';
 export { default as wait } from './wait.js';
