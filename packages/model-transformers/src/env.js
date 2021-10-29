e6a0ca7d4525249d2b15b52827e9156094ee1e6e
Jacob Bare <jacob@parameter1.com>
2021-10-29 09:40:48 -0500

    Add default asset and image host env vars

--- packages/model-transformers/src/env.js
+++ packages/model-transformers/src/env.js
@@ -4,9 +4,13 @@ import {
 } from 'envalid';
 
 export const {
+  CDN_ASSET_HOSTNAME,
+  CDN_IMAGE_HOSTNAME,
   LEGACY_MONGO_URL,
   MONGO_URL,
 } = cleanEnv(process.env, {
+  CDN_ASSET_HOSTNAME: str({ desc: 'The default asset hostname', default: 'cdn.base.parameter1.com' }),
+  CDN_IMAGE_HOSTNAME: str({ desc: 'The default image hostname', default: 'p1-cms-assets.imgix.net' }),
   LEGACY_MONGO_URL: str({ desc: 'The Base Platform (legacy) MongoDB server to connect to.' }),
   MONGO_URL: str({ desc: 'The Base Platform (legacy) MongoDB server to connect to.' }),
 });
