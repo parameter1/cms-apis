f145712038d30b49a6d17d000a8fb243f39c6398
Jacob Bare <jacob@parameter1.com>
2021-10-29 09:41:05 -0500

    Add `websiteById` query and additional Website fields

--- packages/model-transformers/src/graphql/resolvers/website.js
+++ packages/model-transformers/src/graphql/resolvers/website.js
@@ -1,12 +1,65 @@
 import { trim } from '@cms-apis/utils';
+import { CDN_ASSET_HOSTNAME, CDN_IMAGE_HOSTNAME } from '../../env.js';
 
 export default {
   /**
    *
    */
   Website: {
+    description({ description }) {
+      return trim(description);
+    },
+    fullName({ fullName }) {
+      return trim(fullName);
+    },
+    hosts(content) {
+      const image = trim(content.imageHost);
+      const asset = trim(content.assetHost);
+      return {
+        root: trim(content.host),
+        image: image || CDN_IMAGE_HOSTNAME,
+        asset: asset || CDN_ASSET_HOSTNAME,
+      };
+    },
+    logo({ logo }) {
+      return trim(logo);
+    },
     name({ name }) {
       return trim(name);
+    },
+    origin(site) {
+      const host = trim(site.host);
+      return host ? `https://${host}` : null;
+    },
+    shortName({ shortName }) {
+      return trim(shortName);
+    },
+    status({ status }) {
+      return parseInt(status, 10) || null;
+    },
+    tagLine({ tagLine }) {
+      return trim(tagLine);
+    },
+    title(site) {
+      const name = trim(site.name);
+      const shortName = trim(site.shortName);
+      if (!shortName) return name;
+      return name ? `${name} (${shortName})` : null;
+    },
+    url({ url }) {
+      return trim(url);
+    },
+  },
+
+  /**
+   *
+   */
+  Query: {
+    async websiteById(_, { input }, { loaders }) {
+      const { id } = input;
+      const product = await loaders.get('platform.Product').load(id);
+      if (product.type !== 'Site') throw new Error(`The product returned for ID ${id} is not a website.`);
+      return product;
     },
   },
 };
--- packages/model-transformers/src/graphql/type-defs/website.js
+++ packages/model-transformers/src/graphql/type-defs/website.js
@@ -2,9 +2,36 @@ import gql from '@cms-apis/graphql/tag';
 
 export default gql`
 
+extend type Query {
+  websiteById(input: QueryWebsiteByIdInput!): Website
+}
+
 type Website {
   _id: ObjectID!
   name: String!
+  fullName: String!
+  tagLine: String
+  description: String
+  logo: String
+
+  status: Int!
+
+  url: String
+
+  title: String!
+  shortName: String
+  hosts: WebsiteHosts!
+  origin: String!
+}
+
+type WebsiteHosts {
+  root: String!
+  image: String!
+  asset: String!
+}
+
+input QueryWebsiteByIdInput {
+  id: ObjectID!
 }
 
 `;
