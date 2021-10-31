c0d770825ee287ce41014904c305032d0ed8fd1a
Jacob Bare <jacob@parameter1.com>
2021-10-31 10:08:30 -0500

    Add more content types

--- packages/model-transformers/src/graphql/type-defs/content.js
+++ packages/model-transformers/src/graphql/type-defs/content.js
@@ -55,7 +55,91 @@ type ContentArticle implements ContentInterface @interfaceFields {
   _id: Int!
 }
 
+type ContentBlog implements ContentInterface @interfaceFields {
+  _id: Int!
+}
+
+type ContentCollection implements ContentInterface @interfaceFields {
+  _id: Int!
+}
+
 type ContentCompany implements ContentInterface @interfaceFields {
+  _id: Int!
+}
+
+type ContentContact implements ContentInterface @interfaceFields {
+  _id: Int!
+}
+
+type ContentDocument implements ContentInterface @interfaceFields {
+  _id: Int!
+}
+
+type ContentEBook implements ContentInterface @interfaceFields {
+  _id: Int!
+}
+
+type ContentEvent implements ContentInterface @interfaceFields {
+  _id: Int!
+}
+
+type ContentJob implements ContentInterface @interfaceFields {
+  _id: Int!
+}
+
+type ContentMediaGallery implements ContentInterface @interfaceFields {
+  _id: Int!
+}
+
+type ContentNews implements ContentInterface @interfaceFields {
+  _id: Int!
+}
+
+type ContentPage implements ContentInterface @interfaceFields {
+  _id: Int!
+}
+
+type ContentPodcast implements ContentInterface @interfaceFields {
+  _id: Int!
+}
+
+type ContentPressRelease implements ContentInterface @interfaceFields {
+  _id: Int!
+}
+
+type ContentProduct implements ContentInterface @interfaceFields {
+  _id: Int!
+}
+
+type ContentPromotion implements ContentInterface @interfaceFields {
+  _id: Int!
+}
+
+type ContentSpace implements ContentInterface @interfaceFields {
+  _id: Int!
+}
+
+type ContentSupplier implements ContentInterface @interfaceFields {
+  _id: Int!
+}
+
+type ContentTextAd implements ContentInterface @interfaceFields {
+  _id: Int!
+}
+
+type ContentVenue implements ContentInterface @interfaceFields {
+  _id: Int!
+}
+
+type ContentVideo implements ContentInterface @interfaceFields {
+  _id: Int!
+}
+
+type ContentWebinar implements ContentInterface @interfaceFields {
+  _id: Int!
+}
+
+type ContentWhitepaper implements ContentInterface @interfaceFields {
   _id: Int!
 }
 
