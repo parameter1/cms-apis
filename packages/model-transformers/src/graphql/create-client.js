b6a254918db2daa59ab1a12e2b134c0fb55b4008
Jacob Bare <jacob@parameter1.com>
2021-11-01 15:53:41 -0500

    Auto-generate fragment matching using temp client

--- packages/model-transformers/src/graphql/create-client.js
+++ packages/model-transformers/src/graphql/create-client.js
@@ -1,13 +1,39 @@
 import apolloClient from 'apollo-client';
 import apolloCache from 'apollo-cache-inmemory';
 import link from 'apollo-link-schema';
+import gql from '@cms-apis/graphql/tag';
+import { getAsArray } from '@cms-apis/object-path';
 import schema from './schema.js';
 
 const { ApolloClient } = apolloClient;
-const { InMemoryCache } = apolloCache;
+const { InMemoryCache, IntrospectionFragmentMatcher } = apolloCache;
 const { SchemaLink } = link;
 
 export default async ({ dbs, loaders } = {}) => {
+  const tempClient = new ApolloClient({
+    ssrMode: true,
+    cache: new InMemoryCache(),
+    link: new SchemaLink({ schema }),
+  });
+
+  const { data } = await tempClient.query({
+    query: gql`
+      query {
+        __schema {
+          types {
+            kind
+            name
+            possibleTypes { name }
+          }
+        }
+      }
+    `,
+  });
+
+  const types = getAsArray(data, '__schema.types').filter((type) => type.possibleTypes !== null);
+  const introspectionQueryResultData = { __schema: { types } };
+  const fragmentMatcher = new IntrospectionFragmentMatcher({ introspectionQueryResultData });
+
   const defaults = {
     website: await dbs.legacy.repo('website.Site').findOne({ options: { strict: true } }),
   };
@@ -17,7 +43,7 @@ export default async ({ dbs, loaders } = {}) => {
   });
   return new ApolloClient({
     ssrMode: true,
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({ fragmentMatcher }),
     link: new SchemaLink({
       schema,
       context: { dbs, defaults, loaders },
