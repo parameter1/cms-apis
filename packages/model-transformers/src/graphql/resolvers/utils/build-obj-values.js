69dc5a316114f978d4aa791be3f98fa8f7bfb0eb
Jacob Bare <jacob@parameter1.com>
2021-11-02 10:14:53 -0500

    Do not allow deep setting
    Sub objects should be build separately so that they can return null when all values are null

--- packages/model-transformers/src/graphql/resolvers/utils/build-obj-values.js
+++ packages/model-transformers/src/graphql/resolvers/utils/build-obj-values.js
@@ -1,9 +1,4 @@
-import { set } from '@cms-apis/object-path';
-
 export default (values) => {
   if (values.every(([, value]) => value == null)) return null;
-  return values.reduce((o, [path, value]) => {
-    set(o, path, value);
-    return o;
-  }, {});
+  return values.reduce((o, [key, value]) => ({ ...o, [key]: value }), {});
 };
