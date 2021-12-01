dcde81ce8578bd6c7a3b21aeb97544e556948b91
Jacob Bare <jacob@parameter1.com>
2021-12-01 15:28:32 -0600

    Dasherize field names and build self + related

--- apis/rest/src/utils/create-link-builder.js
+++ apis/rest/src/utils/create-link-builder.js
@@ -1,10 +1,22 @@
+import inflector from 'inflected';
+
+const dasherize = (value) => inflector.dasherize(inflector.underscore(value));
+
 export default ({ req, tenant, endpointVar = 'PERSISTENCE_ENDPOINT' }) => {
   const { constants } = req.app.locals;
   const endpoint = constants[endpointVar];
   const rootUrl = `${req.protocol}://${req.get('host')}${constants.API_PATH}${endpoint}`;
+
+  const linkSelf = ({ id, type, field } = {}) => `${rootUrl}/${type}/${id}/links/${dasherize(field)}?x-tenant-key=${tenant}`;
+  const linkRelated = ({ id, type, field } = {}) => `${rootUrl}/${type}/${id}/${dasherize(field)}?x-tenant-key=${tenant}`;
+
   return {
     self: ({ id, type } = {}) => `${rootUrl}/${type}/${id}?x-tenant-key=${tenant}`,
-    linkSelf: ({ id, type, field } = {}) => `${rootUrl}/${type}/${id}/links/${field}?x-tenant-key=${tenant}`,
-    linkRelated: ({ id, type, field } = {}) => `${rootUrl}/${type}/${id}/${field}?x-tenant-key=${tenant}`,
+    linkSelf,
+    linkRelated,
+    linkage: ({ id, type, field }) => ({
+      self: linkSelf({ id, type, field }),
+      related: linkRelated({ id, type, field }),
+    }),
   };
 };
