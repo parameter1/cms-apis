83ef90f328581f464a555eb15d88004debbaf79d
Jacob Bare <jacob@parameter1.com>
2021-12-01 19:55:11 -0600

    Change _id field to id

--- apis/rest/src/utils/clean-node.js
+++ apis/rest/src/utils/clean-node.js
@@ -4,6 +4,6 @@ import sortKeys from 'sort-keys';
 
 export default (node) => sortKeys(mapObject(node, (key, value) => {
   if (key === '__typename') return mapObjectSkip;
-  if (key === '_id') return [key, LegacyDB.coerceId(value), { shouldRecurse: false }];
+  if (key === 'id') return [key, LegacyDB.coerceId(value), { shouldRecurse: false }];
   return [key, value];
 }, { deep: true }), { deep: true });
