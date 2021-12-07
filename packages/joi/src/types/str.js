dc3746b17b3620d785acfa5460019eb7d50d67ff
Jacob Bare <jacob@parameter1.com>
2021-12-07 15:39:36 -0600

    Handle html, multiline, and html entities

--- packages/joi/src/types/str.js
+++ packages/joi/src/types/str.js
@@ -1,3 +1,6 @@
+import clean from '@cms-apis/clean-string';
+import { getAsArray } from '@cms-apis/object-path';
+
 const isRequired = (helpers) => {
   const presence = helpers.schema.$_getFlag('presence');
   return presence === 'required';
@@ -22,5 +25,48 @@ export default (joi) => ({
       value: value || null,
       ...(required && !value && { errors: helpers.error('any.required') }),
     };
+  },
+  validate(value, helpers) {
+    let v = value;
+    if (!helpers.schema.$_getRule('html')) {
+      // no html rule set. strip all tags
+      // @todo only deconding entities because platform was; re-examine
+      v = clean(v, { stripHtmlTags: true, decodeEntities: true });
+    }
+
+    if (!helpers.schema.$_getFlag('multiline')) {
+      // strip multi-line fields when multiline isn't set
+      v = v.replace(/[\r\n]/g, '__NEW-LINE__')
+        .split('__NEW-LINE__')
+        .map((l) => l.trim())
+        .filter((l) => l)
+        .join(' ');
+    }
+    return { value: v };
+  },
+  rules: {
+    html: {
+      args: [
+        {
+          name: 'params',
+          assert: joi.object({
+            tags: joi.array().items(joi.string().trim()).default([]),
+          }),
+        },
+      ],
+      method(params) {
+        return this.$_addRule({ name: 'html', args: { params } });
+      },
+      validate(value, helpers, { params }) {
+        const tags = getAsArray(params, 'tags');
+        // @todo only deconding entities because platform was; re-examine
+        return clean(value, { stripHtmlTags: tags.length ? tags : false, decodeEntities: true });
+      },
+    },
+    multiline: {
+      method() {
+        return this.$_setFlag('multiline', true);
+      },
+    },
   },
 });
