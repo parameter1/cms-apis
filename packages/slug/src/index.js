b2db8813b44276e39014ab86a22c783a7db6f0c6
Jacob Bare <jacob@parameter1.com>
2021-12-07 09:31:35 -0600

    Add additional conditions and create tests

--- packages/slug/src/index.js
+++ packages/slug/src/index.js
@@ -1,12 +1,27 @@
 import slug from 'slug';
+import { isFunction, isObject, trim } from '@cms-apis/utils';
 
 slug.extend({
   '/': '-',
+  '\\': '-',
   _: '-',
+  '-': '-',
   '&': 'and',
 });
 
 export default (value, options) => {
-  const sluggified = slug(value, options);
-  return sluggified.replace(/-{2,}/g, '-');
+  if (!value) return null;
+  if (isObject(value)) throw new Error('Object values cannot be sluggified');
+  if (isFunction(value)) throw new Error('Function values cannot be sluggified');
+  const trimmed = trim(Array.isArray(value) ? value.join('-') : value);
+  if (!trimmed) return null;
+
+  const sluggified = slug(trimmed, options);
+
+  // remove repetitive dashes
+  const repetitiveRemoved = sluggified.replace(/-{2,}/g, '-');
+  // strip leading and trailing dashes
+  const dashesTrimmed = repetitiveRemoved.replace(/^-+/, '').replace(/-+$/, '');
+  // return null when empty
+  return dashesTrimmed || null;
 };
--- packages/slug/test/index.spec.js
+++ packages/slug/test/index.spec.js
@@ -1,7 +1,78 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
-import index from '../src/index.js';
+import slug from '../src/index.js';
 
-describe('index.js', () => {
-  it('should exist', () => expect(index).to.exist);
+describe('slug', () => {
+  it('should be a function', () => expect(slug).to.a('function'));
+
+  it('should convert null value to null', () => {
+    expect(slug(null)).to.be.null;
+  });
+
+  it('should convert undefined value to null', () => {
+    expect(slug()).to.be.null;
+  });
+
+  it('should convert empty string value to null', () => {
+    expect(slug('')).to.be.null;
+  });
+
+  it('should throw an error when value is an object', () => {
+    expect(() => {
+      slug({});
+    }).to.throw();
+  });
+
+  it('should throw an error when value is a function', () => {
+    expect(() => {
+      slug(() => {});
+    }).to.throw();
+  });
+
+  it('should convert strings with only spaces to null', () => {
+    expect(slug(' ')).to.be.null;
+    expect(slug('  ')).to.be.null;
+  });
+
+  it('should trim whitepace', () => {
+    expect(slug('  foo   ')).to.eq('foo');
+    expect(slug(' foo ')).to.eq('foo');
+  });
+
+  it('should not allow repetitive dashes', () => {
+    expect(slug('foo--bar')).to.eq('foo-bar');
+    expect(slug('Foo     Bar   /  Baz')).to.eq('foo-bar-baz');
+  });
+
+  it('should not allow leading and trailing dashes', () => {
+    expect(slug('--Foo   Bar-- ')).to.eq('foo-bar');
+    expect(slug(' - -Foo   Bar- - ')).to.eq('foo-bar');
+  });
+
+  it('should convert all dashes to null', () => {
+    expect(slug('-')).to.be.null;
+    expect(slug('--')).to.be.null;
+    expect(slug('-- \\ / ----  -')).to.be.null;
+  });
+
+  it('should convert slashes to dashes', () => {
+    expect(slug('Foo/Bar')).to.eq('foo-bar');
+    expect(slug('Foo / Bar')).to.eq('foo-bar');
+    expect(slug('Foo \\ Bar')).to.eq('foo-bar');
+    expect(slug('Foo\\Bar')).to.eq('foo-bar');
+    expect(slug('/Foo\\Bar/')).to.eq('foo-bar');
+    expect(slug('Foo // Bar \\\\ Baz')).to.eq('foo-bar-baz');
+    expect(slug('/// /// //')).to.be.null;
+    expect(slug('\\\\\\')).to.be.null;
+  });
+  it('should convert & to and', () => {
+    expect(slug('foo & bar')).to.eq('foo-and-bar');
+    expect(slug('foo&bar')).to.eq('fooandbar');
+    expect(slug('& foo  &  bar &')).to.eq('and-foo-and-bar-and');
+  });
+  it('should convert underscores to dashes', () => {
+    expect(slug('foo_bar')).to.eq('foo-bar');
+    expect(slug('_foo_  _ __bar_')).to.eq('foo-bar');
+    expect(slug('____')).to.be.null;
+  });
 });
