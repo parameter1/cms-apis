c5eb16700fbea733f8df9aba5aec6ec8878a01ce
Jacob Bare <jacob@parameter1.com>
2021-12-09 11:53:07 -0600

    Export extended joi instance by default

--- packages/joi/src/index.js
+++ packages/joi/src/index.js
@@ -1,2 +1,8 @@
+import Joi from 'joi';
+
+import { objectId, str } from './types/index.js';
+
 export { default as validateAsync } from './validate-async.js';
 export { default as validate } from './validate.js';
+
+export default Joi.extend(objectId).extend(str);
