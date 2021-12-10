9d2c335e04ce8d44a5d5b7c8f4fef39c4feefa52
Jacob Bare <jacob@parameter1.com>
2021-12-10 15:08:39 -0600

    Experiment with website and website section create

--- packages/db/src/data-sources/website-section.js
+++ packages/db/src/data-sources/website-section.js
@@ -1,8 +1,82 @@
+import Joi, { validateAsync } from '@cms-apis/joi';
 import AbstractDataSource from './-abstract.js';
+import { createLinks, oidLinkage, payload as restPayload } from '../fields/rest/index.js';
+import sectionFields from '../fields/models/website-section.js';
+import websiteFields from '../fields/models/website.js';
+import generateSlugFrom from '../fields/utils/generate-slug-from.js';
 
 export default class WebsiteSectionDataSource extends AbstractDataSource {
-  test() {
-    console.log('WebsiteSectionDataSource.test');
+  /**
+   * Once created:
+   *  - push section to `websites._connection.sections`
+   *  - if parent is present
+   *    - push section to related `_connection.descendants`
+   *    - generate `name.full`
+   *    - generate `alias` from `_connection.ancestors` + `slug`
+   *
+   * @param {object} params
+   * @param {object} options
+   * @returns {Promise<object>}
+   */
+  async create(params = {}) {
+    const {
+      name,
+      slug,
+      websiteId,
+    } = await validateAsync(Joi.object({
+      name: sectionFields.name.required(),
+      slug: sectionFields.slug.required(),
+      websiteId: websiteFields.id.required(),
+    }), {
+      ...params,
+      ...(!params.slug && params.name && { slug: generateSlugFrom(params.name) }),
+    });
+
+    const website = await this.dataSources.get('websites').findById({
+      id: websiteId,
+      options: { strict: true, projection: { name: 1 } },
+    });
+
+    const doc = {
+      _connection: {},
+      _edge: {
+        website: { node: website },
+      },
+      name: { default: name },
+      slug,
+    };
+
+    // on create
+    // push section to websites._connection.sections
+    // if parent is present
+
+    console.log(doc);
+
     return this;
+  }
+
+  /**
+   *
+   *
+   * @param {object} params
+   * @returns {Promise<object>}
+   */
+  async createFromRestPayload(params = {}) {
+    const obj = await validateAsync(restPayload({
+      name: sectionFields.name.required(),
+      alias: Joi.string().trim(),
+      links: createLinks({
+        site: oidLinkage.required(),
+      }),
+    }), params);
+
+    const { links } = obj;
+    const slug = obj.alias ? obj.alias.split('/').pop() : null;
+
+    return this.create({
+      name: obj.name,
+      slug,
+      websiteId: links.site.linkage.id,
+    });
   }
 }
--- packages/db/src/data-sources/website.js
+++ packages/db/src/data-sources/website.js
@@ -1,9 +1,63 @@
+import Joi, { validateAsync } from '@cms-apis/joi';
 import AbstractDataSource from './-abstract.js';
 
 export default class WebsiteDataSource extends AbstractDataSource {
-  test() {
-    console.log('Website Test that calls section test');
-    this.dataSources.get('website-sections').test();
+  /**
+   *
+   * @param {object} params
+   * @returns {Promise<object>}
+   */
+  async create(params = {}) {
+    const {
+      abbreviation,
+      assetHost,
+      description,
+      host,
+      imageHost,
+      name,
+    } = await validateAsync(Joi.object({
+      abbreviation: Joi.str().default(null),
+      assetHost: Joi.str().domain({ tlds: { allow: true } }).default('cdn.base.parameter1.com'),
+      description: Joi.str().default(null),
+      host: Joi.str().domain({ tlds: { allow: true } }).required(),
+      imageHost: Joi.str().domain({ tlds: { allow: true } }).default('p1-cms-assets.imgix.net'),
+      name: Joi.str().required(),
+    }), params);
+
+    const doc = {
+      _connection: {
+        scheduleOptions: [], // should auto create standard option?
+        sections: [], // should auto create home section?
+      },
+      abbreviation,
+      description,
+      host: {
+        asset: assetHost,
+        root: host,
+      },
+      imageHost,
+      name,
+      origin: `https://${host}`,
+    };
+
+    console.log(doc);
+
     return this;
+  }
+
+  /**
+   *
+   * @param {object} params
+   * @returns {Promise<object>}
+   */
+  async createFromRestPayload(params = {}) {
+    const obj = await validateAsync(Joi.object().unknown().required(), params);
+    return this.create({
+      assetHost: obj.assetHost,
+      description: obj.description,
+      host: obj.host,
+      imageHost: obj.imageHost,
+      name: obj.name,
+    });
   }
 }
