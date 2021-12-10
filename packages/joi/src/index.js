dba227a2ee91a8b09cd6492c75b7a2bc8de54330
Jacob Bare <jacob@parameter1.com>
2021-12-10 15:07:30 -0600

    Add slug to joi instance

--- packages/joi/src/index.js
+++ packages/joi/src/index.js
@@ -4,10 +4,16 @@ import {
   float,
   integer,
   objectId,
+  slug,
   str,
 } from './types/index.js';
 
 export { default as validateAsync } from './validate-async.js';
 export { default as validate } from './validate.js';
 
-export default Joi.extend(float).extend(integer).extend(objectId).extend(str);
+export default Joi
+  .extend(float)
+  .extend(integer)
+  .extend(objectId)
+  .extend(str)
+  .extend(slug);
