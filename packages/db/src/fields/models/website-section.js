d59078f355262b12ae8a8f4f1909bba17b8afc2e
Jacob Bare <jacob@parameter1.com>
2021-12-11 08:38:25 -0600

    Add description field

--- packages/db/src/fields/models/website-section.js
+++ packages/db/src/fields/models/website-section.js
@@ -1,9 +1,10 @@
 import Joi from '@cms-apis/joi';
-import { limited as limitedHtml } from '../html.js';
 import integerId from '../integer-id.js';
+import { limited as limitedHtml, full as fullHtml } from '../html.js';
 
 export default {
   id: integerId,
+  description: fullHtml,
   name: limitedHtml,
   slug: Joi.slug(),
 };
