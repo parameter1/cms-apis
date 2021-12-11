11c975b18fa2003006b193d581397c9da84c4333
Jacob Bare <jacob@parameter1.com>
2021-12-11 08:38:21 -0600

    Use correct website section id type

--- packages/db/src/fields/models/website-section.js
+++ packages/db/src/fields/models/website-section.js
@@ -1,8 +1,9 @@
 import Joi from '@cms-apis/joi';
 import { limited as limitedHtml } from '../html.js';
+import integerId from '../integer-id.js';
 
 export default {
-  id: Joi.objectId(),
+  id: integerId,
   name: limitedHtml,
   slug: Joi.slug(),
 };
