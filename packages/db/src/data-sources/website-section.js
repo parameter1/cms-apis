62f5d46eccb1e68dd35f1b0ce74545d1c5f91183
Jacob Bare <jacob@parameter1.com>
2021-12-11 10:18:46 -0600

    Handle parent website section

--- packages/db/src/data-sources/website-section.js
+++ packages/db/src/data-sources/website-section.js
@@ -1,6 +1,14 @@
+import { Repo } from '@cms-apis/mongodb';
 import Joi, { validateAsync } from '@cms-apis/joi';
+import { get, getAsArray } from '@cms-apis/object-path';
+import { inspect } from 'util';
 import AbstractDataSource from './-abstract.js';
-import { createLinks, oidLinkage, payload as restPayload } from '../fields/rest/index.js';
+import {
+  createLinks,
+  intLinkage,
+  oidLinkage,
+  payload as restPayload,
+} from '../fields/rest/index.js';
 import sectionFields from '../fields/models/website-section.js';
 import websiteFields from '../fields/models/website.js';
 import generateSlugFrom from '../fields/utils/generate-slug-from.js';
@@ -20,11 +28,15 @@ export default class WebsiteSectionDataSource extends AbstractDataSource {
    */
   async create(params = {}) {
     const {
+      description,
       name,
+      parentId,
       slug,
       websiteId,
     } = await validateAsync(Joi.object({
+      description: sectionFields.description.default(null),
       name: sectionFields.name.required(),
+      parentId: sectionFields.id.default(null),
       slug: sectionFields.slug.required(),
       websiteId: websiteFields.id.required(),
     }), {
@@ -32,25 +44,67 @@ export default class WebsiteSectionDataSource extends AbstractDataSource {
       ...(!params.slug && params.name && { slug: generateSlugFrom(params.name) }),
     });
 
-    const website = await this.dataSources.get('websites').findById({
-      id: websiteId,
-      options: { strict: true, projection: { name: 1 } },
-    });
+    // @todo dataloader?
+    const [website, parent] = await Promise.all([
+      this.dataSources.get('websites').findById({
+        id: websiteId,
+        options: { strict: true, projection: { name: 1 } },
+      }),
+      parentId ? this.findById({
+        id: parentId,
+        options: {
+          strict: true,
+          projection: {
+            name: 1,
+            slug: 1,
+            alias: 1,
+            depth: 1,
+            '_edge.website.node._id': 1,
+            '_connection.ancestors': 1,
+          },
+        },
+      }) : null,
+    ]);
 
+    if (parent) {
+      const parentWebsiteId = get(parent, '_edge.website.node._id');
+      if (`${parentWebsiteId}` !== `${website._id}`) {
+        throw Repo.createError(400, `The parent section's website ID (${parentWebsiteId}) does not match this section's website ID (${website._id})`);
+      }
+    }
+    const parentNode = parent ? { _id: parent._id, alias: parent.alias, name: parent.name } : null;
+
+    // @todo do incoming values need to be sanitized/hydrated???
     const doc = {
-      _connection: {},
+      _connection: {
+        ancestors: parent ? [
+          ...getAsArray(parent, '_connection.ancestors'),
+          { depth: parent.alias.split('/').length, node: parentNode },
+        ] : [],
+        descendants: [],
+        related: [],
+      },
       _edge: {
+        coverImage: null, // @todo
+        logo: null, // @todo
+        parent: parent ? { node: parentNode } : null,
         website: { node: website },
       },
-      name: { default: name },
+      alias: parent ? `${parent.alias}/${slug}` : slug,
+      depth: parent ? parent.depth + 1 : 1,
+      description,
+      metadata: {}, // @todo
+      name: {
+        default: name,
+        full: parent ? `${parent.name.full} > ${name}` : name,
+      },
+      redirects: [], // @todo
+      seo: null, // @todo
+      sequence: 0, // @todo
       slug,
     };
 
-    // on create
-    // push section to websites._connection.sections
-    // if parent is present
-
-    console.log(doc);
+    console.log(inspect(doc, { colors: true, depth: 5 }));
 
     return this;
   }
@@ -66,6 +120,7 @@ export default class WebsiteSectionDataSource extends AbstractDataSource {
       name: sectionFields.name.required(),
       alias: Joi.string().trim(),
       links: createLinks({
+        parent: intLinkage,
         site: oidLinkage.required(),
       }),
     }), params);
@@ -75,8 +130,9 @@ export default class WebsiteSectionDataSource extends AbstractDataSource {
 
     return this.create({
       name: obj.name,
+      parentId: get(links, 'parent.linkage.id'),
       slug,
-      websiteId: links.site.linkage.id,
+      websiteId: get(links, 'site.linkage.id'),
     });
   }
 }
