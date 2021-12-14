39bbbfde82ee1ef0e2d6f6550197017b66a581a5
Jacob Bare <jacob@parameter1.com>
2021-12-14 13:45:44 -0600

    Create prepare document util
    That omits/cleans values and sorts keys

---
+++ packages/document-hash/src/prepare-document.js
@@ -0,0 +1,48 @@
+import { DB, ObjectId } from '@cms-apis/db';
+import { get, set } from '@cms-apis/object-path';
+import sortKeys from 'sort-keys';
+import mapObject, { mapObjectSkip } from 'map-obj';
+import is from '@sindresorhus/is';
+import preparePathsToHash from './prepare-paths-to-hash.js';
+
+export default (doc, paths = []) => {
+  if (!doc) throw new Error('No document was provided');
+  const toHash = preparePathsToHash(paths);
+  const o = {
+    _id: DB.coerceId(doc._id),
+  };
+  if (!o._id) throw new Error('Unable to extract a document _id value');
+  toHash.forEach((path) => {
+    set(o, path, get(doc, path));
+  });
+
+  const mapped = mapObject(o, (key, value, source) => {
+    if (value == null) return mapObjectSkip;
+    if (is.date(value)) return [key, value.valueOf()];
+    if (is.string(value) || is.number(value) || is.boolean(value)) return [key, value];
+    if (is.array(value)) {
+      if (!value.length) return mapObjectSkip;
+      if (is.array(value, is.number) || is.array(value, is.string) || is.array(value, is.boolean)) {
+        return [key, value.sort()];
+      }
+      throw new Error('Sorting non-scalar or mixed typed arrays is not yet supported');
+    }
+    if (is.function(value.toString) && /^[a-f0-9]{24}$/.test(`${value}`)) return [key, `${value}`];
+    if (is.plainObject(value)) {
+      if (is.emptyObject(value)) return mapObjectSkip;
+      return [key, value];
+    }
+    if (is.directInstanceOf(value, ObjectId)) return [key, `${value}`];
+    const error = new Error(`Unsupported ${is(value)} type encountered for key ${key}`);
+    error.key = key;
+    error.value = value;
+    error.source = source;
+    throw error;
+  }, { deep: true });
+
+  // need to run map again to clear any empty objects that were created from dot access
+  return sortKeys(mapObject(mapped, (key, value) => {
+    if (is.emptyObject(value)) return mapObjectSkip;
+    return [key, value];
+  }, { deep: true }), { deep: true });
+};
---
+++ packages/document-hash/test/prepare-document.spec.js
@@ -0,0 +1,163 @@
+/* eslint-disable import/no-extraneous-dependencies, no-unused-expressions */
+import { ObjectId } from '@cms-apis/db';
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+import prepare from '../src/prepare-document.js';
+
+describe('prepare-document', () => {
+  it('should throw an error when no document is provided', () => {
+    expect(() => {
+      prepare();
+    }).to.throw('No document was provided');
+  });
+  it('should throw an error when an _id cannot be extracted', () => {
+    expect(() => {
+      prepare({});
+    }).to.throw('Unable to extract a document _id value');
+  });
+  it('should omit unspecified paths', () => {
+    const v = { foo: 'bar', baz: 'dill' };
+    const expected = { foo: 'bar' };
+    const result = prepare({ _id: 1, v, o: { v } }, ['v.foo', 'o.v.foo']);
+    expect(result).to.deep.equal({ _id: 1, v: expected, o: { v: expected } });
+  });
+  it('should omit null values', () => {
+    const v = null;
+    const result = prepare({ _id: 1, v, o: { v } }, ['v', 'o.v']);
+    expect(result).to.deep.equal({ _id: 1 });
+  });
+  it('should omit undefined values', () => {
+    const v = undefined;
+    const result = prepare({ _id: 1, v, o: { v } }, ['v', 'o.v']);
+    expect(result).to.deep.equal({ _id: 1 });
+  });
+  it('should omit empty arrays', () => {
+    const v = [];
+    const result = prepare({ _id: 1, v, o: { v } }, ['v', 'o.v']);
+    expect(result).to.deep.equal({ _id: 1 });
+  });
+  it('should omit empty objects', () => {
+    const v = {};
+    const result = prepare({ _id: 1, v, o: { v } }, ['v', 'o.v']);
+    expect(result).to.deep.equal({ _id: 1 });
+  });
+  it('should convert dates', () => {
+    const v = new Date(1639508844407);
+    const expected = 1639508844407;
+    const result = prepare({ _id: 1, v, o: { v } }, ['v', 'o.v']);
+    expect(result).to.deep.equal({ _id: 1, v: expected, o: { v: expected } });
+  });
+  it('should convert ObjectIds', () => {
+    const v = new ObjectId('61b8ed6ef10eafd26b54b5c3');
+    const expected = '61b8ed6ef10eafd26b54b5c3';
+    const result = prepare({ _id: 1, v, o: { v } }, ['v', 'o.v']);
+    expect(result).to.deep.equal({ _id: 1, v: expected, o: { v: expected } });
+  });
+  it('should convert ObjectId-like objects', () => {
+    const v = { toString: () => '61b8ed6ef10eafd26b54b5c3' };
+    const expected = '61b8ed6ef10eafd26b54b5c3';
+    const result = prepare({ _id: 1, v, o: { v } }, ['v', 'o.v']);
+    expect(result).to.deep.equal({ _id: 1, v: expected, o: { v: expected } });
+  });
+  it('should convert ObjectIds', () => {
+    const v = new ObjectId('61b8ed6ef10eafd26b54b5c3');
+    const expected = '61b8ed6ef10eafd26b54b5c3';
+    const result = prepare({ _id: 1, v, o: { v } }, ['v', 'o.v']);
+    expect(result).to.deep.equal({ _id: 1, v: expected, o: { v: expected } });
+  });
+  it('should preserve strings, numbers, and booleans', () => {
+    const v = { str: 'str', num: 3, bool: true };
+    const result = prepare({ _id: 1, v, o: { v } }, ['v', 'o.v']);
+    expect(result).to.deep.equal({ _id: 1, v, o: { v } });
+  });
+  it('should sort arrays with numbers', () => {
+    const v = [3, 1, 5, 75, -1];
+    const expected = [-1, 1, 3, 5, 75];
+    const result = prepare({ _id: 1, v, o: { v } }, ['v', 'o.v']);
+    expect(result).to.deep.equal({ _id: 1, v: expected, o: { v: expected } });
+  });
+  it('should sort arrays with strings', () => {
+    const v = ['3', '1', '5', '75', '-1'];
+    const expected = ['-1', '1', '3', '5', '75'];
+    const result = prepare({ _id: 1, v, o: { v } }, ['v', 'o.v']);
+    expect(result).to.deep.equal({ _id: 1, v: expected, o: { v: expected } });
+  });
+  it('should sort arrays with booleans', () => {
+    const v = [false, true, false];
+    const expected = [false, false, true];
+    const result = prepare({ _id: 1, v, o: { v } }, ['v', 'o.v']);
+    expect(result).to.deep.equal({ _id: 1, v: expected, o: { v: expected } });
+  });
+  it('should throw an error when a non-supported type is encountered', () => {
+    expect(() => {
+      prepare({ _id: 1, v: () => {} }, ['v']);
+    }).to.throw('Unsupported Function type encountered for key v');
+    expect(() => {
+      prepare({ _id: 1, v: new Map() }, ['v']);
+    }).to.throw('Unsupported Map type encountered for key v');
+    expect(() => {
+      prepare({ _id: 1, v: NaN }, ['v']);
+    }).to.throw('Unsupported number type encountered for key v');
+    expect(() => {
+      prepare({ _id: 1, v: /foo/ }, ['v']);
+    }).to.throw('Unsupported RegExp type encountered for key v');
+  });
+  it('should throw an error when non-string, number, or boolean arrays are encountered', () => {
+    expect(() => {
+      prepare({ _id: 1, v: [{ _id: 2 }] }, ['v']);
+    }).to.throw('Sorting non-scalar or mixed typed arrays is not yet supported');
+    expect(() => {
+      prepare({ _id: 1, v: [1, '2', false] }, ['v']);
+    }).to.throw('Sorting non-scalar or mixed typed arrays is not yet supported');
+  });
+  it('should sort the keys', () => {
+    const value = {
+      _id: 1,
+      c: 3,
+      a: 1,
+      b: { c: 1, a: 1 },
+    };
+    const result = prepare(value, ['c', 'a', 'b']);
+    expect(result).to.deep.equal({
+      _id: 1,
+      a: 1,
+      b: { a: 1, c: 1 },
+      c: 3,
+    });
+  });
+
+  it('should handle the kitchen sink', () => {
+    const result = prepare({
+      _id: 1234,
+      name: 'foo',
+      oid: new ObjectId('61b8ed6ef10eafd26b54b5c3'),
+      date: new Date(1639508844407),
+      obj: { b: 1, a: 2, c: null },
+      deep: {
+        arr2: [3, 1, 5, 75, -1],
+        arr1: [],
+        arr3: [],
+        o: {},
+      },
+    }, [
+      'date',
+      'name',
+      'body',
+      'oid',
+      'links.social',
+      'links.website',
+      'obj',
+      'deep.arr2',
+      'deep.arr1',
+      'deep.o.foo',
+    ]);
+    expect(result).to.deep.equal({
+      _id: 1234,
+      date: 1639508844407,
+      deep: { arr2: [-1, 1, 3, 5, 75] },
+      name: 'foo',
+      obj: { a: 2, b: 1 },
+      oid: '61b8ed6ef10eafd26b54b5c3',
+    });
+  });
+});
