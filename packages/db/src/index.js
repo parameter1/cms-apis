96d60b111c08e6296bee58978fa5532e738b2426
Jacob Bare <jacob@parameter1.com>
2021-12-15 15:37:41 -0600

    Create document cleaner utility
    Similar to the document hash preparation function, but without converting dates and object IDs, and also not touching underscore fields

--- packages/db/src/index.js
+++ packages/db/src/index.js
@@ -1,4 +1,6 @@
 export { ObjectId, MongoDBClient, iterateMongoCursor } from '@cms-apis/mongodb';
+export { mapObjectSkip } from 'map-obj';
 export { DB, LegacyDB } from './repo/index.js';
 export { default as DataSources } from './data-sources/index.js';
 export { default as types } from './types.js';
+export { default as cleanDocument } from './clean-document.js';
