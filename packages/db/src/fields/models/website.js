7fff47b06ebf3cffeff8df482661fd98899a576e
Jacob Bare <jacob@parameter1.com>
2021-12-15 13:37:04 -0600

    Add abbreviation and host fields

--- packages/db/src/fields/models/website.js
+++ packages/db/src/fields/models/website.js
@@ -2,6 +2,10 @@ import Joi from '@cms-apis/joi';
 
 export default {
   id: Joi.objectId(),
+  abbreviation: Joi.string(),
+  assetHost: Joi.hostname(),
+  imageHost: Joi.hostname(),
+  host: Joi.hostname(),
   description: Joi.string(),
   name: Joi.string(),
 };
