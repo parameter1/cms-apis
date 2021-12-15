9e3e8e9f7b6a5795416d3b26f8984116240188ec
Jacob Bare <jacob@parameter1.com>
2021-12-15 14:44:36 -0600

    Add create helper functions and website retrieve

--- packages/db/src/data-sources/-abstract.js
+++ packages/db/src/data-sources/-abstract.js
@@ -5,6 +5,44 @@ export default class AbstractDataSource {
     this.dataSources = dataSources;
   }
 
+  static buildOnCreateFields({ date = new Date(), context } = {}) {
+    const user = AbstractDataSource.getUserFromContext(context);
+    // @todo sort and clean these fields!
+    return {
+      _meta: AbstractDataSource.buildOnCreateMeta({ date, user }),
+      _version: AbstractDataSource.buildOnCreateVersion({ date, user }),
+    };
+  }
+
+  static buildOnCreateMeta({ date, user }) {
+    return {
+      created: { date, by: user },
+      updated: { date, by: user },
+    };
+  }
+
+  static buildOnCreateVersion({ date, user }) {
+    return {
+      n: 1,
+      history: [{ date, by: user }],
+    };
+  }
+
+  static getUserFromContext(context = {}) {
+    return context.user ? {
+      _id: context.user._id,
+      username: context.user.username,
+      name: context.user.name,
+      email: context.user.email,
+    } : {
+      _id: 'system.core',
+    };
+  }
+
+  loadWebsite({ id, projection = {}, strict = true } = {}) {
+    return this.dataSources.get('websites').load({ value: id, projection, strict });
+  }
+
   load(...args) {
     return this.loader.load(...args);
   }
